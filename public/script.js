7e6a861d06e4eca8ee6a86efe032f68b0c3900b9
Maya Novarini <Maya@Admins-MacBook-Pro.local>
2017-12-02 23:54:59 -0800

    add item additional hiding logic for the cart

--- public/script.js
+++ public/script.js
@@ -3,16 +3,31 @@ new Vue({
   data: {
     total: 0,
     items: [
-      { title: 'Item 1' },
-      { title: 'Item 2' },
-      { title: 'Item 3' },
+      { id: 1, title: 'Item 1' },
+      { id: 2, title: 'Item 2' },
+      { id:3, title: 'Item 3' },
   ],
     cart: []
   },
   methods: {
     addItem: function(index){
       this.total += 9.99;
-      this.cart.push(this.items[index]);
+      let item = this.items[index];
+      let found = false;
+      for (var i = 0; i < this.cart.length; i++){
+        if (this.cart[i].id === item.id) {
+          found = true;
+          this.cart[i].qty++;
+        }
+      }
+      if (!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          qty: 1
+        });
+      }
+      
     }
   }
 });
